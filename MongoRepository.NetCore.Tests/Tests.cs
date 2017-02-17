59df6164e45d71c058071569c7b4da710900a26b
auc-cloud-dev <auc.cloud.dev@gmail.com>
2017-02-17 16:22:51 -0700

    Add .Net Core project and test project

---
+++ MongoRepository.NetCore.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MongoRepository.NetCore;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Class1 clazz = new Class1();
+            Assert.True(true);
+        }
+    }
+}
